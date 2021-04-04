41520351c382164a9be677c5794fae5d31d4507a
carlos-ch <carlos.tschavez@gmail.com>
2021-04-04 05:40:13 +0200

    change prop name

--- src/components/TextCard/TextCard.js
+++ src/components/TextCard/TextCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Input.css';
 
-const TextCard = ({ cardText, buttonType, userName }) => {
+const TextCard = ({ cardText, buttonText, userName }) => {
   return (
     <div className="card-wrapper">
       <p className="card-text">{cardText}</p>
-      <button className="card-btn">{buttonType}</button>
+      <button className="card-btn">{buttonText}</button>
       <span className="card-name">{userName}</span>
     </div>
   );
