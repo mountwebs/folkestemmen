a7cf07d55e428aacae8e0a9b12e40438b41cc33a
mountwebs <mountwebs@gmail.com>
2021-04-05 13:01:24 +0200

    edited welcome

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,7 +2,16 @@ import React from "react";
 import styles from "./Welcome.module.css";
 
 const Welcome = () => {
-  return <div className={styles.welcome}></div>;
+  return (
+    <div className={styles.welcome}>
+      <div className={styles.content}>
+        <h1>Velkommen til Folkestemmen!</h1>
+        <div>
+          Gi tilbakemelding og kom med innspill på spørsmål fra kommunen din{" "}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
