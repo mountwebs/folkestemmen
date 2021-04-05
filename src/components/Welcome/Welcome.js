e6958ddb785c87473664ded78780593b6aec278f
mountwebs <mountwebs@gmail.com>
2021-04-05 15:10:42 +0200

    welcome css

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -4,9 +4,9 @@ import styles from "./Welcome.module.css";
 const Welcome = () => {
   return (
     <div className={styles.welcome}>
-      <div className={styles.content}>
-        <h1>Velkommen til Folkestemmen!</h1>
-        <div>
+      <div className={styles.container}>
+        <h1 className={styles.header}>Velkommen til Folkestemmen!</h1>
+        <div className={styles.description}>
           Gi tilbakemelding og kom med innspill på spørsmål fra kommunen din{" "}
         </div>
       </div>
