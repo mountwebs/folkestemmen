5885959bbf40ba0281aca02f45ff24fb44080588
mountwebs <mountwebs@gmail.com>
2021-04-05 15:38:50 +0200

    improved css

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -6,9 +6,9 @@ const Welcome = () => {
     <div className={styles.welcome}>
       <div className={styles.container}>
         <h1 className={styles.header}>Velkommen til Folkestemmen!</h1>
-        <div className={styles.description}>
+        <p className={styles.description}>
           Gi tilbakemelding og kom med innspill på spørsmål fra kommunen din{" "}
-        </div>
+        </p>
       </div>
     </div>
   );
