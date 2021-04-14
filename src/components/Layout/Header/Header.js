de94e572a73032029a373348abaa41481d501b22
mountwebs <mountwebs@gmail.com>
2021-04-14 22:57:59 +0200

    conditional login render in welcome and made header styled component

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,18 +1,47 @@
 import React from "react";
 import styles from "./Header.module.css";
 import logoFull from "../../../assets/logo-full.svg";
+import styled from "styled-components";
+import Button from "../../Button/Button";
+import device from "../../../constants/breakpoints";
+
+const StyledButton = styled(Button)`
+  font-size: 18px;
+  margin-top: 2em;
+
+  @media only screen and ${device.sm} {
+    display: none;
+  }
+`;
+
+const StyledHeader = styled.header`
+  height: 103px;
+  width: 100%;
+`;
+
+const StyledContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledLogo = styled.img`
+  flex-grow: 1;
+  max-width: 500px;
+  margin: 10px 57.5px;
+`;
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <img
-          className={styles.logo}
-          src={logoFull}
-          alt="Beta folkestemmen - logo"
-        />
-      </div>
-    </header>
+    <StyledHeader>
+      <StyledContainer>
+        <StyledLogo src={logoFull} alt="Beta folkestemmen - logo" />
+        <StyledButton primary icon="PersonFill">
+          Logg inn
+        </StyledButton>
+      </StyledContainer>
+    </StyledHeader>
   );
 };
 
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./Welcome.module.css";
 import Button from "../Button/Button";
-import styled from 'styled-components';
+import styled from "styled-components";
+import device from "../../constants/breakpoints";
 
 const StyledButton = styled(Button)`
   font-size: 18px;
-`
+  margin-top: 2em;
+
+  @media only screen and ${device.sm} {
+    display: none;
+  }
+`;
 
 const Welcome = () => {
   return (
@@ -15,7 +21,9 @@ const Welcome = () => {
         <p className={styles.description}>
           Gi tilbakemelding og kom med innspill på spørsmål fra kommunen din{" "}
         </p>
-        <StyledButton primary icon="PersonFill">Logg inn</StyledButton>
+        <StyledButton primary icon="PersonFill">
+          Logg inn
+        </StyledButton>
       </div>
     </div>
   );
