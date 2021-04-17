5b378794e26217758ba9c272746edfb28dbdc652
mountwebs <mountwebs@gmail.com>
2021-04-17 17:25:13 +0200

    added welcome image

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '../Button/Button';
 import styled from 'styled-components';
 import device from '../../constants/breakpoints';
+import welcomeImg from '../../assets/welcome.svg';
 
 const StyledWelcome = styled.div`
   background: #f2f2f2;
@@ -10,11 +11,21 @@ const StyledWelcome = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+
+  @media only screen and ${device.sm} {
+    align-items: stretch;
+    height: 536px;
+  }
 `;
 
 const StyledContainer = styled.div`
   text-align: center;
   padding: 40px;
+
+  @media only screen and ${device.sm} {
+    padding-top: 70px;
+  }
 `;
 
 const StyledHeader = styled.h1`
@@ -36,6 +47,24 @@ const StyledButton = styled(Button)`
   }
 `;
 
+const StyledWelcomeImg = styled.img`
+  display: none;
+
+  @media only screen and ${device.sm} {
+    display: block;
+    position: absolute;
+    margin: auto;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 0;
+    height: 310px;
+    max-width: 100%;
+    object-fit: cover;
+    align-self: flex-end;
+  }
+`;
+
 const Welcome = () => {
   return (
     <StyledWelcome>
@@ -47,6 +76,7 @@ const Welcome = () => {
         <StyledButton primary icon="PersonFill">
           Logg inn
         </StyledButton>
+        <StyledWelcomeImg src={welcomeImg} />
       </StyledContainer>
     </StyledWelcome>
   );
