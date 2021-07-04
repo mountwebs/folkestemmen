d62204bfb00ca435a5b89206dffdb6ed44481071
mountwebs <mountwebs@gmail.com>
2021-07-04 16:18:41 +0200

    added basic text to footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -2,12 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
-  height: 100px;
   width: 100%;
   background: #292929;
   position: relative;
   bottom: 0;
   color: #a5a5a5;
+  padding: 3rem;
 `;
 
 const StyledContainer = styled.div`
@@ -20,15 +20,20 @@ const StyledContainer = styled.div`
 const StyledImageContainer = styled.div`
   height: 50px;
   width: 50px;
+  margin-bottom: 30px;
 `;
 
-const StyledTextContainer = styled.div``;
+const StyledTextContainer = styled.div`
+  max-width: 300px;
+`;
 
 const StyledTitle = styled.h3`
   margin: 0;
 `;
 
-const StyledText = styled.div``;
+const StyledText = styled.div`
+  font-size: 1rem;
+`;
 
 const Footer = () => {
   return (
@@ -36,8 +41,10 @@ const Footer = () => {
       <StyledContainer>
         <StyledImageContainer></StyledImageContainer>
         <StyledTextContainer>
-          <StyledTitle>Test</StyledTitle>
-          <StyledText>Testing text bla bla bla</StyledText>
+          <StyledTitle>Beta kommune</StyledTitle>
+          <StyledText>
+            Design og digital samfunnsutvikling fra innbyggernes perspektiv
+          </StyledText>
         </StyledTextContainer>
       </StyledContainer>
     </StyledFooter>
