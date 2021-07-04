8f9c7cca8aa4b1c6d8cc8d7cda914c4542ddc62e
mountwebs <mountwebs@gmail.com>
2021-07-04 16:08:21 +0200

    Styled footer and made containers

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,8 +1,47 @@
-import React from "react";
-import styles from "./Footer.module.css";
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  height: 100px;
+  width: 100%;
+  background: #292929;
+  position: relative;
+  bottom: 0;
+  color: #a5a5a5;
+`;
+
+const StyledContainer = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledImageContainer = styled.div`
+  height: 50px;
+  width: 50px;
+`;
+
+const StyledTextContainer = styled.div``;
+
+const StyledTitle = styled.h3`
+  margin: 0;
+`;
+
+const StyledText = styled.div``;
 
 const Footer = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <StyledFooter>
+      <StyledContainer>
+        <StyledImageContainer></StyledImageContainer>
+        <StyledTextContainer>
+          <StyledTitle>Test</StyledTitle>
+          <StyledText>Testing text bla bla bla</StyledText>
+        </StyledTextContainer>
+      </StyledContainer>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
