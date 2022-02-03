dbad8d0ca0f6de4ef6efccc6a9434f5700cf101f
mountwebs <mountwebs@gmail.com>
2022-02-03 23:00:25 +0100

    started user context

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,5 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const UserContext = React.createContext()
+//https://stackoverflow.com/questions/62504525/persistence-with-localstorage-with-usestate-and-usecontext-react-hooks
 
-export default UserContext
+var initialState = {
+  userId: null,
+  likes: [],
+};
+
+function setLocalStorage(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    console.log('Error at setting local storage');
+    // catch possible errors:
+    // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+  }
+}
+
+function getLocalStorage(key, initialValue) {
+  try {
+    const value = window.localStorage.getItem(key);
+    return value ? JSON.parse(value) : initialValue;
+  } catch (e) {
+    console.log('Error at getting local storage');
+    return initialValue;
+  }
+}
+
+const UserContext = React.createContext();
+
+function UserProvider({ children }) {
+  const [user, setUser] = useState(() => getLocalStorage('user', initialState));
+
+  useEffect(() => {
+    setLocalStorage('user', user);
+  }, [user]);
+
+  return (
+    <UserContext.Provider
+      value={{
+        userId: null,
+        likes: [],
+        setUserId: (userId) => setUser({ ...user, userId }),
+        setLikes: (likes) => setUser({ ...user, likes }),
+        setPosts: (posts) => setUser({ ...user, posts }),
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export default UserContext;
+
+export { UserProvider };
