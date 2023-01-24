a17112667dae6b5ea567af4304035fc03188c1eb
mountwebs <mountwebs@gmail.com>
2023-01-24 20:13:08 +0100

    Feat: karlsøy url

--- src/components/AnswerBoard/AnswerBoard.jsx
+++ src/components/AnswerBoard/AnswerBoard.jsx
@@ -89,7 +89,7 @@ const StyledLoadMoreButton = styled(Button)`
 
 const ANSWERS_LIMIT = 25;
 
-const baseUrl = 'https://folkestemmen-api-tokke.onrender.com/';
+const baseUrl = 'https://folkestemmen-api-karlsoy.onrender.com/';
 
 const AnswerBoard = () => {
   const [answerList, setAnswerList] = useState('');
