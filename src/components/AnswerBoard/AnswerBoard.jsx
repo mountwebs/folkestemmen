b03246617e42d5fcf56d47934d6e3d20fd7a8f66
mountwebs <mountwebs@gmail.com>
2023-01-29 15:18:11 +0100

    chore: update api url

--- src/components/AnswerBoard/AnswerBoard.jsx
+++ src/components/AnswerBoard/AnswerBoard.jsx
@@ -89,7 +89,7 @@ const StyledLoadMoreButton = styled(Button)`
 
 const ANSWERS_LIMIT = 25;
 
-const baseUrl = 'https://folkestemmen-api-tokke.onrender.com/';
+const baseUrl = 'https://stiangk.dev/api/tokke/';
 
 const AnswerBoard = () => {
   const [answerList, setAnswerList] = useState('');
